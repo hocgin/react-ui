9d866fbc3d9d8f487db3f5a96a3f13b7c3d7f848
hocgin <hocgin@gmail.com>
2021-07-16 21:24:36 +0800

    feat:

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -5,16 +5,13 @@ export default {
   disableTypeCheck: true,
   cjs: { type: 'babel', lazy: true },
   esm: { type: 'babel' },
-  // cssModules: false,
-  extractCSS: true,
   lessInBabelMode: true,
-  // runtimeHelpers: true,
   extraRollupPlugins: [image()],
-  extraBabelPlugins: [
-    ['babel-plugin-import', {
-      libraryName: 'antd',
-      libraryDirectory: 'es',
-      style: true,
-    }],
-  ],
+  // extraBabelPlugins: [
+  //   ['babel-plugin-import', {
+  //     libraryName: 'antd',
+  //     libraryDirectory: 'es',
+  //     style: true,
+  //   }],
+  // ],
 };
