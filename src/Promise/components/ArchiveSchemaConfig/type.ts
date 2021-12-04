46f631093178ee8eacc8032dd609d4473366b332
hocgin <hocgin@gmail.com>
2021-12-04 11:41:10 +0800

    feat: v4.0.21

--- src/Promise/components/ArchiveSchemaConfig/type.ts
+++ src/Promise/components/ArchiveSchemaConfig/type.ts
@@ -1,4 +1,4 @@
 export interface UseAction {
-  initialValues: (params: Record<string, any>) => Promise<any>;
+  initialValues?: (params: Record<string, any>) => Promise<any>;
   submit: (params: Record<string, any>) => Promise<any>;
 }
