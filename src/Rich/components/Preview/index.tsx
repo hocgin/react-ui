52189de9b9ffb9b319fe5635fe9a1cd87f6b1a35
hocgin <hocgin@gmail.com>
2021-12-14 11:17:32 +0800

    feat: v4.0.33

--- src/Rich/components/Preview/index.tsx
+++ src/Rich/components/Preview/index.tsx
@@ -1,37 +1,24 @@
 import React, { Component } from 'react';
 import styles from './index.less';
 import { Utils } from '@/index';
+import { useMount } from 'ahooks';
 
-interface EditorProps {
+const Index: React.FC<{
   children?: string;
-}
+}> = ({ children }, ref) => {
+  useMount(() => {
+    if (Utils.Lang.isServer()) {
+      return;
+    }
+    require('braft-extensions/dist/code-highlighter.css');
+  });
 
-interface EditorState {}
-
-class Index extends Component<EditorProps, EditorState> {
-  static defaultProps = {
-    children: '',
-  };
-
-  constructor(props: any, context: any) {
-    super(props, context);
-  }
-
-  componentDidMount() {}
-
-  render() {
-    Utils.Lang.tryRequire('braft-extensions/dist/code-highlighter.css');
-    return (
-      <div className={styles.richPreview}>
-        <div dangerouslySetInnerHTML={this.dangerouslySetInnerHTML} />
-      </div>
-    );
-  }
-
-  get dangerouslySetInnerHTML() {
-    let { children } = this.props;
-    return { __html: `${children}` };
-  }
-}
+  let dangerouslySetInnerHTML = { __html: `${children}` };
+  return (
+    <div className={styles.richPreview}>
+      <div dangerouslySetInnerHTML={dangerouslySetInnerHTML} />
+    </div>
+  );
+};
 
 export default Index;
