d7ee4cfb1c007684f33972e19f4ff7e519217417
hocgin <hocgin@gmail.com>
2021-12-26 19:27:15 +0800

    feat: v4.0.39

--- src/Header/components/Header/index.tsx
+++ src/Header/components/Header/index.tsx
@@ -1,59 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classnames from 'classnames';
 import { MenuOutlined, CloseOutlined } from '@ant-design/icons';
 import styles from './index.less';
 
-interface LogoProps {
+const Index: React.FC<{
   menus?: any[];
   className?: string | undefined;
-}
-
-interface LogoState {
-  isOpenMenu?: boolean;
-}
-
-class Index extends React.Component<LogoProps, LogoState> {
-  static defaultProps = {
-    menus: [],
-  };
-  state = {
-    isOpenMenu: false,
-  };
-
-  render() {
-    let { isOpenMenu } = this.state;
-    let { menus, className } = this.props;
-    return (
-      <header className={classnames(styles.topHeader, className)}>
-        <a href="http://www.hocgin.top/" className={styles.logo}>
-          HOCGIN
-          <span className={styles.suffix}>
-            <span className={styles.dot}>.</span>top
-          </span>
-        </a>
-        <div
-          className={classnames(styles.toggle, { [styles.active]: isOpenMenu })}
-          onClick={this.onClickToggle}
-        >
-          {isOpenMenu ? <CloseOutlined /> : <MenuOutlined />}
-        </div>
-        <ul
-          className={classnames(styles.navigation, {
-            [styles.active]: isOpenMenu,
-          })}
-        >
-          {(menus || []).map(({ href, title }, index) => (
-            <li key={index}>
-              <a href={href}>{title}</a>
-            </li>
-          ))}
-        </ul>
-      </header>
-    );
-  }
-
-  onClickToggle = () =>
-    this.setState(({ isOpenMenu }) => ({ isOpenMenu: !isOpenMenu }));
-}
+}> = ({ className, menus = [] }, ref) => {
+  let [isOpenMenu, setIsOpenMenu] = useState(false);
+  return (
+    <header className={classnames(styles.topHeader, className)}>
+      <a href="http://www.hocgin.top/" className={styles.logo}>
+        <span>HOCGIN</span>
+        <span className={styles.suffix}>
+          <span className={styles.dot}>.</span>
+          <span>top</span>
+        </span>
+      </a>
+      <div
+        className={classnames(styles.toggle, { [styles.active]: isOpenMenu })}
+        onClick={() => setIsOpenMenu(!isOpenMenu)}
+      >
+        {isOpenMenu ? <CloseOutlined /> : <MenuOutlined />}
+      </div>
+      <ul
+        className={classnames(styles.navigation, {
+          [styles.active]: isOpenMenu,
+        })}
+      >
+        {(menus || []).map(({ href, title }, index) => (
+          <li key={index}>
+            <a href={href}>{title}</a>
+          </li>
+        ))}
+      </ul>
+    </header>
+  );
+};
 
 export default Index;
