bf1b89052465ac122b6322e365e2b8d6cf0444d6
hocgin <hocgin@gmail.com>
2022-03-18 19:51:07 +0800

    [新增功能](v4.0.46): 调整

--- src/Header/components/Header/index.tsx
+++ src/Header/components/Header/index.tsx
@@ -8,44 +8,67 @@ type Mode = 'none' | 'fixed' | 'sticky';
 
 const Index: React.FC<{
   menus?: any[];
-  mode?: Mode,
+  mode?: Mode;
   className?: string | undefined;
+  style?: any;
+  containerClassName?: string | undefined;
+  containerStyle?: any;
   title?: React.ReactNode | string;
   href?: string;
-  style?: any;
-}> = ({ className, style, mode = 'none', menus = [], title, href = 'https://www.hocgin.top' }, ref) => {
+}> = ({
+  className,
+  style,
+  containerClassName,
+  containerStyle,
+  mode = 'none',
+  menus = [],
+  title,
+  href = 'https://www.hocgin.top',
+}) => {
   let [isOpenMenu, setIsOpenMenu] = useState(false);
 
   return (
-    <header className={classnames(styles.topHeader, {
-      [styles.fixed]: mode === 'fixed',
-      [styles.sticky]: mode === 'sticky',
-    }, className)} style={style}>
-      <a href={href} className={styles.logo}>
-        <span>HOCGIN</span>
-        <span className={styles.suffix}>
-          <span className={styles.dot}>.</span>
-          <span>top</span>
-        </span>
-        {title && <span className={styles.title}>{title}</span>}
-      </a>
+    <header
+      className={classnames(
+        styles.header,
+        {
+          [styles.fixed]: mode === 'fixed',
+          [styles.sticky]: mode === 'sticky',
+        },
+        className,
+      )}
+      style={style}
+    >
       <div
-        className={classnames(styles.toggle, { [styles.active]: isOpenMenu })}
-        onClick={() => setIsOpenMenu(!isOpenMenu)}
+        className={classnames(styles.container, containerClassName)}
+        style={containerStyle}
       >
-        {isOpenMenu ? <CloseOutlined /> : <MenuOutlined />}
+        <a href={href} className={styles.logo}>
+          <span>HOCGIN</span>
+          <span className={styles.suffix}>
+            <span className={styles.dot}>.</span>
+            <span>top</span>
+          </span>
+          {title && <span className={styles.title}>{title}</span>}
+        </a>
+        <div
+          className={classnames(styles.toggle, { [styles.active]: isOpenMenu })}
+          onClick={() => setIsOpenMenu(!isOpenMenu)}
+        >
+          {isOpenMenu ? <CloseOutlined /> : <MenuOutlined />}
+        </div>
+        <ul
+          className={classnames(styles.navigation, {
+            [styles.active]: isOpenMenu,
+          })}
+        >
+          {(menus || []).map(({ href, title }, index) => (
+            <li key={index}>
+              <a href={href}>{title}</a>
+            </li>
+          ))}
+        </ul>
       </div>
-      <ul
-        className={classnames(styles.navigation, {
-          [styles.active]: isOpenMenu,
-        })}
-      >
-        {(menus || []).map(({ href, title }, index) => (
-          <li key={index}>
-            <a href={href}>{title}</a>
-          </li>
-        ))}
-      </ul>
     </header>
   );
 };
