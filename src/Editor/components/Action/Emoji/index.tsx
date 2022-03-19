ccc6173dcd3beba521b2efdb43cf1b2b6cb084aa
hocgin <hocgin@gmail.com>
2022-03-19 22:26:37 +0800

    [新增功能](main): 打包

--- src/Editor/components/Action/Emoji/index.tsx
+++ src/Editor/components/Action/Emoji/index.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import { Editor } from '@tiptap/react';
 import TbButton from '@/Editor/components/TbButton';
-import { BoldOutlined, SmileOutlined } from '@ant-design/icons';
+import { SmileOutlined } from '@ant-design/icons';
 import { Popover } from 'antd';
+// @ts-ignore
 import { Picker } from 'emoji-mart';
 
 export const Emoji: React.FC<{ editor?: Editor | null }> = ({
--- typings.d.ts
+++ typings.d.ts
@@ -1,6 +1,2 @@
-declare module 'mockjs';
-declare module 'emoji-mart';
-declare module 'react-color';
-
 declare module '*.css';
 declare module '*.less';
