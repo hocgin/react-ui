b9b94486fe1aa9b6f90ba9915719c46d39b73ae6
hocgin <hocgin@gmail.com>
2022-09-04 23:38:03 +0800

    add update

---
+++ src/Promise/components/SelectLbs/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+/**
+ * 地图选点
+ * 下拉输入文字搜索 点击弹窗选点
+ * - https://github.com/ElemeFE/react-amap
+ * - https://gitee.com/hocgin/HyWeixinMP
+ * - https://github.com/uiwjs/react-amap
+ * @constructor
+ */
+const Index: React.FC<{}> = ({}) => {
+  return <div></div>
+}
+
+export default Index;
