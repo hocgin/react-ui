3b156a98d80ee741c31e55070267d0f5e11465a5
hocgin <hocgin@gmail.com>
2022-11-16 22:42:12 +0800

    [新增功能](develop): 新增组件

--- src/Adverts/Adverts.tsx
+++ src/Adverts/Adverts.tsx
@@ -5,17 +5,25 @@ import { Carousel, Image } from 'antd';
 
 export interface AdvertItemType {
   title?: string;
+  flag?: string;
   imageUrl: string;
   url: string;
 }
 
 export let defaultAdver = [{
   title: '随机',
+  flag: '推荐',
   imageUrl: 'https://cdn.hocgin.top/uPic/favicon.ico',
   url: 'http://www.baidu.com',
 }, {
   title: '随机',
+  flag: '作者推荐',
   imageUrl: 'https://cdn.hocgin.top/file/addone-bookmarking_nav-background.png',
+  url: 'http://www.baidu.com',
+}, {
+  title: '随机',
+  flag: '作者推荐',
+  imageUrl: 'https://cdn.hocgin.top/file/20221116_tuijian.png',
   url: 'http://www.baidu.com',
 }];
 
@@ -28,18 +36,13 @@ const Index: React.FC<{
 }> = ({ height = 80, width = '100%', items = defaultAdver, ...props }) => {
   let { getPrefixCls } = React.useContext(ConfigContext);
   let prefixCls = getPrefixCls('adverts', props.prefixCls);
-  const onChange = (currentSlide: number) => {
-    console.log(currentSlide);
-  };
-
-
   return (
     <div className={classnames(`${prefixCls}`)} style={{ width, height }}>
-      <Carousel afterChange={onChange} autoplay effect='fade'>
-        {(items || []).map(({ imageUrl, url, title }) => <>
+      <Carousel autoplay effect='fade'>
+        {(items || []).map(({ imageUrl, url, flag = '推广', title }) => <>
           <a href={url}>
             <div className={`${prefixCls}-item`} style={{ width, height }}>
-              <div className={`${prefixCls}-flag`}>推广</div>
+              <div className={`${prefixCls}-flag`}>{flag}</div>
               <Image className={`${prefixCls}-image`} preview={false} src={imageUrl} />
             </div>
           </a>
