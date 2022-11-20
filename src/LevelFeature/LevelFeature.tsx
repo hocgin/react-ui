e8a02110ed58be9a54957ed9c78c32fa48fd1ff6
hocgin <hocgin@gmail.com>
2022-11-20 23:56:31 +0800

    [新增功能](develop): 新增组件

---
+++ src/LevelFeature/LevelFeature.tsx
@@ -0,0 +1,127 @@
+import React from 'react';
+import { ConfigContext } from '@/ConfigProvider';
+import classnames from 'classnames';
+import { CheckCircleOutlined, CheckCircleFilled } from '@ant-design/icons';
+
+export interface FeatureType {
+  title: any;
+  checked?: boolean;
+}
+
+export interface LinkButtonType {
+  title: any;
+  href?: string;
+}
+
+export interface ItemType {
+  title?: string;
+  themeColor?: string;
+  badge?: any[];
+  price?: any[] | any;
+  feature?: FeatureType[];
+  link?: LinkButtonType[];
+}
+
+const Index: React.FC<{
+  prefixCls?: string;
+  className?: string;
+  item?: ItemType;
+}> = ({ item, className, ...props }) => {
+  let { getPrefixCls } = React.useContext(ConfigContext);
+  let prefixCls = getPrefixCls('LevelFeature', props.prefixCls);
+  let featureList = item?.feature ?? [];
+  let linkList = item?.link ?? [];
+
+  return (
+    <div
+      className={classnames(`${prefixCls}`, className)}
+      style={{
+        backgroundColor: item?.themeColor ?? 'rgb(61,64,72)',
+      }}
+    >
+      <div>
+        {/*标题*/}
+        {item?.title && (
+          <h3 className={classnames(`${prefixCls}-title`)}>{item?.title}</h3>
+        )}
+        {/*徽章*/}
+        {item?.badge?.length > 0 && (
+          <div className={`${prefixCls}-badge`}>
+            {(item?.badge || []).map((name) => (
+              <span className={classnames(`${prefixCls}-badge-item`)}>
+                <span className={classnames(`${prefixCls}-badge-text`)}>
+                  {name}
+                </span>
+              </span>
+            ))}
+          </div>
+        )}
+        {/*价格*/}
+        {typeof item?.price === 'string' && (
+          <div className={`${prefixCls}-price`}>
+            <div className={classnames(`${prefixCls}-price-item`)}>
+              {item?.price}
+            </div>
+          </div>
+        )}
+        {typeof item?.price === 'object' && (
+          <div
+            className={`${prefixCls}-price`}
+            style={{
+              display: 'grid',
+              gridTemplateColumns: `repeat(${item?.price.length}, minmax(0, 1fr))`,
+            }}
+          >
+            {(item?.price || []).map((title) => (
+              <span
+                className={classnames(
+                  `${prefixCls}-price-item`,
+                  `${prefixCls}-price-arrayitem`,
+                )}
+              >
+                {title}
+              </span>
+            ))}
+          </div>
+        )}
+      </div>
+      {/*功能介绍*/}
+      {featureList.length > 0 && (
+        <ul className={`${prefixCls}-features`}>
+          {featureList.map(({ title, checked }) => (
+            <ol
+              className={classnames(`${prefixCls}-feature`, {
+                [`checked`]: checked,
+              })}
+            >
+              {checked ? <CheckCircleFilled /> : <CheckCircleOutlined />}{' '}
+              <span>{title}</span>
+            </ol>
+          ))}
+        </ul>
+      )}
+      {/* 按钮 */}
+      {linkList.length > 0 && (
+        <div
+          className={`${prefixCls}-link`}
+          style={{
+            display: 'grid',
+            gridTemplateColumns: `repeat(${linkList.length}, minmax(0, 1fr))`,
+          }}
+        >
+          {(linkList || []).map(({ title, href }) => (
+            <a
+              target="_blank"
+              className={classnames(`${prefixCls}-link-item`)}
+              href={href}
+            >
+              {title}
+            </a>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Index;
---
+++ src/LevelFeature/index.md
@@ -0,0 +1,154 @@
+---
+# @formatter:off
+title: LevelFeature - 功能级别
+nav:
+  title: 组件
+  path: /components
+group:
+  path: /
+# @formatter:on
+---
+
+# Tpl - 模版文件
+
+这是一个组件的基础描述
+
+## 代码演示
+
+### 基础用法
+
+```jsx
+import React from 'react';
+import { Row, Col } from 'antd';
+import { PropertySafetyOutlined } from '@ant-design/icons';
+import { LevelFeature, ConfigProvider } from '@hocgin/ui';
+
+let freeItem = {
+  title: 'Free',
+  badge: ['FREE UPDATES INCLUDED'],
+  price: '$0',
+  feature: [
+    {
+      checked: true,
+      title: 'Save tab & tab groups to new session',
+    },
+    {
+      title: 'Save tab & tab gr session',
+    },
+    {
+      title: 'Save tagrops to new session',
+    },
+  ],
+  link: [
+    {
+      title: '详情',
+    },
+  ],
+};
+let proItem = {
+  title: 'Pro',
+  themeColor: '#0c98eb',
+  badge: [
+    'FREE UPDATES INCLUDED',
+    <>
+      <PropertySafetyOutlined />
+      &nbsp;限时折扣
+    </>,
+  ],
+  price: [
+    <>
+      $10<small>/月</small>
+    </>,
+    <>
+      $20<small>/年</small>
+    </>,
+  ],
+  feature: [
+    {
+      checked: true,
+      title: 'Save tab & tab groups to new sessionSave tab & tab groups to',
+    },
+    {
+      checked: true,
+      title: 'Save tab & tab gr session',
+    },
+    {
+      checked: true,
+      title: 'Save tagrops to new session',
+    },
+  ],
+  link: [
+    {
+      title: '立即购买',
+    },
+    {
+      title: '查看更多',
+    },
+  ],
+};
+let ssvipItem = {
+  title: 'SSVip',
+  themeColor: '#E65B62',
+  badge: ['FREE UPDATES', <>👑&nbsp;尊贵会员</>],
+  price: [
+    <>
+      $30<small>/月</small>
+    </>,
+  ],
+  feature: [
+    {
+      checked: true,
+      title: 'Save tab & tab groups ',
+    },
+    {
+      checked: true,
+      title: 'Save tab & tab gr session',
+    },
+    {
+      checked: true,
+      title: 'Save tagrops to new session',
+    },
+    {
+      checked: true,
+      title: 'Save tab & tab gr session',
+    },
+    {
+      checked: true,
+      title: 'Save tagrops to new session',
+    },
+    {
+      checked: true,
+      title: 'Save tab & tab gr session',
+    },
+    {
+      checked: true,
+      title: 'Save tagrops to new session',
+    },
+  ],
+  link: [
+    {
+      title: '立即购买',
+    },
+  ],
+};
+
+export default () => {
+  return (
+    <>
+      <Row>
+        <LevelFeature className="ok" item={freeItem} />
+        <LevelFeature className="ok" item={proItem} />
+        <LevelFeature className="ok" item={ssvipItem} />
+      </Row>
+    </>
+  );
+};
+```
+
+## API
+
+### 组件
+
+| 参数 | 说明 | 类型 | 默认值 | 版本 |
+| ---- | ---- | ---- | ------ | ---- |
+| -    | -    | -    | -      | -    |
