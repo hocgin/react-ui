af652334984e559c880343009122d11d8d1d8630
hocgin <hocgin@gmail.com>
2022-12-20 12:01:16 +0000

    Deploying to gh-pages from main @ 806fdaaa09a89d86794ac52290f6a8ae75ee5806 🚀

---
+++ Editor__demos.9d83a48b.async.js
@@ -0,0 +1,76 @@
+"use strict";(self.webpackChunk_hocgin_ui=self.webpackChunk_hocgin_ui||[]).push([[6241],{93839:function(B,d,n){n.r(d);var p=n(46686),i=n.n(p),l=n(93236),C=n(36770),_=n(48126),x=n(26674),m=n(36171),o=n(98818),y=n(62144),e=n(62086),D=`
+              <pre class='line-numbers'><code class='language-javascript'>for (var i=1; i <= 20; i++)
+{
+  if (i % 15 == 0)
+    console.log("FizzBuzz");
+  else if (i % 3 == 0)
+    console.log("Fizz");
+  else if (i % 5 == 0)
+    console.log("Buzz");
+  else
+    console.log(i);
+}</code></pre>
+      <h1 name='\u4F60\u597D' data-name='h1' active>\u4F60\u597Dh11</h1>
+        <h2 name='\u4F60\u597D' data-name='h1' active>\u4F60\u597Dh22</h2>
+          <h3 name='\u4F60\u597D' data-name='h1' active>\u4F60\u597Dh33</h3>
+        <h2 name='\u4F60\u597D' data-name='h1' active>\u4F60\u597Dh24</h2>
+          <h3 name='\u4F60\u597D' data-name='h1' active>\u4F60\u597Dh35</h3>
+      <h1 name='\u4F60\u597D' data-name='h1' active>\u4F60\u597Dh16</h1>
+        <h4 name='\u4F60\u597D' data-name='h1' active>\u4F60\u597Dh47</h4>
+
+      <a href='https://www.baidu.com'>ksjdkHi</a>
+
+      <iframe src='https://www.youtube.com/embed/XIMLoLxmTDw' frameborder='0' allowfullscreen></iframe>
+      #FB5151
+      <p><img src='https://source.unsplash.com/8xznAGy4HcY/800x400' width='100' height='50'/></p>
+        <table style='width:100%'>
+    <tr>
+      <th>Firstname</th>
+      <th>Lastname</th>
+      <th>Age</th>
+    </tr>
+    <tr>
+      <td>Jill</td>
+      <td>Smith</td>
+      <td>50</td>
+    </tr>
+    <tr>
+      <td>Eve</td>
+      <td>Jackson</td>
+      <td>94</td>
+    </tr>
+    <tr>
+      <td>John</td>
+      <td>Doe</td>
+      <td>80</td>
+    </tr>
+  </table>
+      <h2>
+        Hi there,
+      </h2>
+      <p>
+        this is a <em>basic</em> example of <strong>tiptap</strong>. Sure, there are all kind of basic text styles you\u2019d probably expect from a text editor. But wait until you see the lists:
+      </p>
+      <ul>
+        <li>
+          That\u2019s a bullet list with one \u2026
+        </li>
+        <li>
+          \u2026 or two list items.
+        </li>
+      </ul>
+      <p>
+        Isn\u2019t that great? And all of that is editable. But wait, there\u2019s more. Let\u2019s try a code block:
+      </p>
+      <pre><code class='language-css'>body {
+  display: none;
+}</code></pre>
+      <p>
+        I know, I know, this is impressive. It\u2019s only the tip of the iceberg though. Give it a try and click a little bit around. Don\u2019t forget to check the other examples too.
+      </p>
+      <blockquote>
+        Wow, that\u2019s amazing. Good work, boy! \u{1F44F}
+        <br />
+        \u2014 Mom
+      </blockquote>
+    `,F=function(h){return["Lea Thompson","Cyndi Lauper","Tom Cruise","Madonna","Jerry Hall","Joan Collins","Winona Ryder","Christina Applegate","Alyssa Milano","Molly Ringwald","Ally Sheedy","Debbie Harry","Olivia Newton-John","Elton John","Michael J. Fox","Axl Rose","Emilio Estevez","Ralph Macchio","Rob Lowe","Jennifer Grey","Mickey Rourke","John Cusack","Matthew Broderick","Justine Bateman","Lisa Bonet"].filter(function(u){return u.toLowerCase().startsWith(h.toLowerCase())}).slice(0,5)};d.default=function(){var a=(0,l.useRef)(),h=(0,l.useState)(!0),u=i()(h,2),c=u[0],w=u[1],O=(0,l.useState)(!1),g=i()(O,2),s=g[0],f=g[1],L=(0,y.Z)(!1),v=i()(L,2),E=v[0],T=v[1].toggle,k=(0,l.useState)(""),b=i()(k,2),j=b[0],M=b[1];console.log("LangKit.matchHtmlTag(content, 'h[1-6]')",C.Z.matchHtmlTag(D,"h[1-6]"));var P={height:E?"unset":void 0};return(0,e.jsxs)(e.Fragment,{children:[(0,e.jsx)(_.Z,{header:s?(0,e.jsx)(x.Z,{style:{padding:"0 10px"},menus:[{label:(0,e.jsx)("a",{href:"www.baidu.com",children:"\u{1F430} \u6807\u9898"})},{label:(0,e.jsx)("a",{href:"www.baidu.com",children:"\u{1F58D}\uFE0F \u6807\u9898"})}]}):(0,e.jsx)(e.Fragment,{}),onSearchMention:F,editorRef:a,editable:c,fullscreen:s,placeholder:"\u8BF7\u8F93\u5165\u5185\u5BB9",value:D,onChangeFullscreen:function(t){return f(t)}}),(0,e.jsx)(m.Z,{}),(0,e.jsx)(o.Z,{onClick:function(){M(a.current.getHTML())},children:"\u83B7\u53D6HTML"}),(0,e.jsx)(o.Z,{onClick:function(){M(JSON.stringify(a.current.getJSON()))},children:"\u83B7\u53D6JSON"}),(0,e.jsx)(o.Z,{onClick:function(){var t=!c;a.current.setEditable(t),w(t)},children:c?"\u53EF\u7F16\u8F91":"\u4E0D\u53EF\u7F16\u8F91"}),(0,e.jsx)(o.Z,{onClick:T,children:E?"\u53D6\u6D88\u9AD8\u5EA6":"\u6062\u590D\u9AD8\u5EA6"}),(0,e.jsx)(o.Z,{onClick:function(){var t=!s;a.current.setFullscreen(t),f(t)},children:s?"\u5168\u5C4F":"\u975E\u5168\u5C4F"}),(0,e.jsx)(m.Z,{}),(0,e.jsx)("div",{children:j}),(0,e.jsx)(_.Z,{editable:!1,value:"\u4F60\u597D"})]})}}}]);
